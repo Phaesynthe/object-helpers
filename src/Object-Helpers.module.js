2aceeacbc36ac127b81e44f02d5bfeccc67928c7
Matthew W. Jackson <mwj8410@gmail.com>
2017-11-24 21:32:11 -0600

    merge: a deep merge operation similar to Object.assign

--- src/Object-Helpers.module.js
+++ src/Object-Helpers.module.js
@@ -1,23 +1,9 @@
-/* global module, require */
+/* global module */
+'use strict'
+
+
 
 const ObjectHelpers = {
-
-  /**
-   * Creates an array of unique keys from n Object's keys.
-   * @param {...object} objectArray A variable number of objects.
-   * @returns {Array} Unique keys expressed in any input object.
-   */
-  indexAttributes: (...objectArray) => {
-    let keySet = new Set();
-
-    objectArray.forEach((obj) => {
-      Object.keys(obj).forEach((key) => {
-        keySet.add(key);
-      })
-    });
-
-    return Array.from(keySet);
-  },
 
   /**
    * Accepts two objects and returns an object describing the changes between the two object. This does not include the values, only a representation of what the change action was. This function does not flatten input objects and only evaluated root attributes. Any child objects are ignored.
@@ -26,27 +12,27 @@ const ObjectHelpers = {
    * @returns {object} An object of key-value pairs where the key represents a single-depth attribute from one or both of the input objects and the value is a string verb indicating what the difference is.
    */
   delta: (oldObject, newObject) => {
-    let delta = {};
-    const keys = ObjectHelpers.indexAttributes(oldObject, newObject);
+    let delta = {}
+    const keys = ObjectHelpers.indexAttributes(oldObject, newObject)
 
     keys.forEach((key) => {
       if (!oldObject[key]) {
         // If the key does not exist in the first object, the value was created
-        delta[key] = 'created';
+        delta[key] = 'created'
       } else if (!newObject[key]) {
         // If the key does not exist in the second object, the value was deleted
-        delta[key] = 'deleted';
+        delta[key] = 'deleted'
       } else if (typeof oldObject[key] === 'object' || typeof newObject[key] === 'object') {
-        delta[key] = 'object value evaluation not supported';
+        delta[key] = 'object value evaluation not supported'
       } else if (oldObject[key] === newObject[key]) {
         // If the key exists in both and the vales are the same, then it was not modified
-        delta[key] = 'un-modified';
+        delta[key] = 'un-modified'
       } else {
         // If the key exists in both and the vales are not the same, then it was updated
-        delta[key] = 'updated';
+        delta[key] = 'updated'
       }
-    });
-    return delta;
+    })
+    return delta
   },
 
   /**
@@ -57,20 +43,58 @@ const ObjectHelpers = {
    * @returns {Object} An object with single depth attributes named based on the input Object's attribute value nesting.
    */
   flatten: (obj, name, stem) => {
-    let out = {};
-    let newStem = (typeof stem !== 'undefined' && stem !== '') ? stem + '_' + name : name;
+    let out = {}
+    let newStem = (typeof stem !== 'undefined' && stem !== '') ? stem + '_' + name : name
 
     if (typeof obj !== 'object') {
-      out[newStem] = obj;
-      return out;
+      out[newStem] = obj
+      return out
     }
 
     for (let attribute in obj) {
-      let prop = ObjectHelpers.flatten(obj[attribute], attribute, newStem);
-      out =  Object.assign({}, out, prop); // ObjectHelpers.merge([out, prop]);
+      let prop = ObjectHelpers.flatten(obj[attribute], attribute, newStem)
+      out =  Object.assign({}, out, prop) // ObjectHelpers.merge([out, prop])
     }
-    return out;
-  }
-};
+    return out
+  },
 
-module.exports = ObjectHelpers;
+  /**
+   * Creates an array of unique keys from n Object's keys.
+   * @param {...object} objectArray A variable number of objects.
+   * @returns {Array} Unique keys expressed in any input object.
+   */
+  indexAttributes: (...objectArray) => {
+    let keySet = new Set()
+
+    objectArray.forEach((obj) => {
+      Object.keys(obj).forEach((key) => {
+        keySet.add(key)
+      })
+    })
+
+    return Array.from(keySet)
+  },
+
+  merge: (objA, objB) => {
+    const outObject = Object.assign({}, objA)
+
+    for (const key in objB) {
+      if (!outObject[key]) {
+        outObject[key] = objB[key]
+      } else if (
+        typeof objB[key] === 'string' ||
+        typeof objB[key] === 'number' ||
+        objB[key] instanceof Date ||
+        objB[key].length
+      ) {
+        outObject[key] = objB[key]
+      } else if (typeof objB[key] === 'object') {
+        outObject[key] = ObjectHelpers.merge(objA[key], objB[key])
+      }
+    }
+
+    return outObject
+  }
+}
+
+module.exports = ObjectHelpers
--- src/Object-Helpers.spec.js
+++ src/Object-Helpers.spec.js
@@ -1,4 +1,5 @@
 /* global after, before, describe, it, require */
+'use strict'
 
 const expect = require('chai').expect;
 
@@ -11,88 +12,7 @@ describe('Object Helpers', () => {
     expect(ObjectHelpers).to.be.a('object');
   });
 
-  describe('indexAttributes', () => {
-
-    it('exists', () => {
-      expect(ObjectHelpers.indexAttributes).to.be.a('function');
-    });
-
-    it('returns an empty array when called with an empty object', () => {
-      let obj = {};
-
-      let result = ObjectHelpers.indexAttributes(obj);
-
-      expect(result).to.be.an('array').to.have.lengthOf(0);
-      expect(ObjectHelpers.indexAttributes).to.be.a('function');
-    });
-
-    it('returns an array with the correct number of keys when called with one object', () => {
-      let obj = {
-        test: 'value'
-      };
-
-      let result = ObjectHelpers.indexAttributes(obj);
-
-      expect(result).to.be.an('array');
-      expect(result).to.be.an('array').to.have.lengthOf(1);
-    });
-
-    it('returns an array with the correct number of keys when called with several objects containing only duplicate keys', () => {
-      let obj1 = {
-        test: 'value1'
-      };
-      let obj2 = {
-        test: 'value2'
-      };
-      let obj3 = {
-        test: 'value3'
-      };
-
-      let result = ObjectHelpers.indexAttributes(obj1, obj2, obj3);
-
-      expect(result).to.be.an('array');
-      expect(result).to.be.an('array').to.have.lengthOf(1);
-    });
-
-    it('returns an array with the correct number of keys when called with several objects containing only unique keys', () => {
-      let obj1 = {
-        test1: 'value1'
-      };
-      let obj2 = {
-        test2: 'value2'
-      };
-      let obj3 = {
-        test3: 'value3'
-      };
-
-      let result = ObjectHelpers.indexAttributes(obj1, obj2, obj3);
-
-      expect(result).to.be.an('array');
-      expect(result).to.be.an('array').to.have.lengthOf(3);
-    });
-
-    it('returns an array with the correct number of keys when called with several objects containing some overlapping keys', () => {
-      let obj1 = {
-        test1: 'value1'
-      };
-      let obj2 = {
-        test1: 'value1',
-        test2: 'value2'
-      };
-      let obj3 = {
-        test2: 'value2',
-        test3: 'value3'
-      };
-
-      let result = ObjectHelpers.indexAttributes(obj1, obj2, obj3);
-
-      expect(result).to.be.an('array');
-      expect(result).to.be.an('array').to.have.lengthOf(3);
-    });
-
-  });
-
-  describe('delta', () => {
+  describe('delta()', () => {
 
     it('exists', () => {
       expect(ObjectHelpers.delta).to.be.a('function');
@@ -174,7 +94,7 @@ describe('Object Helpers', () => {
 
   });
 
-  describe('flatten', () => {
+  describe('flatten()', () => {
 
     it('exists', () => {
       expect(ObjectHelpers.flatten).to.be.a('function');
@@ -221,5 +141,178 @@ describe('Object Helpers', () => {
     });
 
   });
+
+  describe('indexAttributes()', () => {
+
+    it('exists', () => {
+      expect(ObjectHelpers.indexAttributes).to.be.a('function');
+    });
+
+    it('returns an empty array when called with an empty object', () => {
+      let obj = {};
+
+      let result = ObjectHelpers.indexAttributes(obj);
+
+      expect(result).to.be.an('array').to.have.lengthOf(0);
+      expect(ObjectHelpers.indexAttributes).to.be.a('function');
+    });
+
+    it('returns an array with the correct number of keys when called with one object', () => {
+      let obj = {
+        test: 'value'
+      };
+
+      let result = ObjectHelpers.indexAttributes(obj);
+
+      expect(result).to.be.an('array');
+      expect(result).to.be.an('array').to.have.lengthOf(1);
+    });
+
+    it('returns an array with the correct number of keys when called with several objects containing only duplicate keys', () => {
+      let obj1 = {
+        test: 'value1'
+      };
+      let obj2 = {
+        test: 'value2'
+      };
+      let obj3 = {
+        test: 'value3'
+      };
+
+      let result = ObjectHelpers.indexAttributes(obj1, obj2, obj3);
+
+      expect(result).to.be.an('array');
+      expect(result).to.be.an('array').to.have.lengthOf(1);
+    });
+
+    it('returns an array with the correct number of keys when called with several objects containing only unique keys', () => {
+      let obj1 = {
+        test1: 'value1'
+      };
+      let obj2 = {
+        test2: 'value2'
+      };
+      let obj3 = {
+        test3: 'value3'
+      };
+
+      let result = ObjectHelpers.indexAttributes(obj1, obj2, obj3);
+
+      expect(result).to.be.an('array');
+      expect(result).to.be.an('array').to.have.lengthOf(3);
+    });
+
+    it('returns an array with the correct number of keys when called with several objects containing some overlapping keys', () => {
+      let obj1 = {
+        test1: 'value1'
+      };
+      let obj2 = {
+        test1: 'value1',
+        test2: 'value2'
+      };
+      let obj3 = {
+        test2: 'value2',
+        test3: 'value3'
+      };
+
+      let result = ObjectHelpers.indexAttributes(obj1, obj2, obj3);
+
+      expect(result).to.be.an('array');
+      expect(result).to.be.an('array').to.have.lengthOf(3);
+    });
+
+  });
+
+  describe('merge()', () => {
+
+    it('existis', () => {
+      expect(ObjectHelpers.merge).to.be.a('function');
+    })
+
+    it('returns the first object when called with only one object', () => {
+      const result = ObjectHelpers.merge({ test: 'value'})
+      expect(Object.keys(result).length).to.equal(1)
+      expect(result.test).to.equal('value')
+    })
+
+    it('merges new string values in', () => {
+      const result = ObjectHelpers.merge({ test: 'value'} , { merged: 'string'})
+      expect(Object.keys(result).length).to.equal(2)
+      expect(result.test).to.equal('value')
+      expect(result.merged).to.equal('string')
+    })
+
+    it('merges new number values in', () => {
+      const result = ObjectHelpers.merge({ test: 'value'} , { merged: 1 })
+      expect(Object.keys(result).length).to.equal(2)
+      expect(result.test).to.equal('value')
+      expect(result.merged).to.equal(1)
+    })
+
+    it('merges new number float values in', () => {
+      const result = ObjectHelpers.merge({ test: 'value'} , { merged: 1.2 })
+      expect(Object.keys(result).length).to.equal(2)
+      expect(result.test).to.equal('value')
+      expect(result.merged).to.equal(1.2)
+    })
+
+    it('merges new date values in', () => {
+      const dateValue = new Date()
+      const result = ObjectHelpers.merge({ test: 'value'} , { merged: dateValue})
+      expect(Object.keys(result).length).to.equal(2)
+      expect(result.test).to.equal('value')
+      expect(result.merged.toISOString()).to.equal(dateValue.toISOString())
+    })
+
+    it('merges new arrays in', () => {
+      const result = ObjectHelpers.merge({ test: 'value'} , { merged: ['test']})
+      expect(Object.keys(result).length).to.equal(2)
+      expect(result.test).to.equal('value')
+      expect(result.merged.length).to.equal(1)
+      expect(result.merged[0]).to.equal('test')
+    })
+
+    it('merges new objects in', () => {
+      const result = ObjectHelpers.merge({ test: 'value'} , { merged: { test: 'value2' } })
+      expect(Object.keys(result).length).to.equal(2)
+      expect(result.test).to.equal('value')
+      expect(typeof result.merged).to.equal('object')
+      expect(result.merged.test).to.equal('value2')
+    })
+
+    it('replaces string values in', () => {
+      const result = ObjectHelpers.merge({ test: 'value'} , { test: 'string'})
+      expect(Object.keys(result).length).to.equal(1)
+      expect(result.test).to.equal('string')
+    })
+
+    it('merges number values in', () => {
+      const result = ObjectHelpers.merge({ test: 'value'} , { test: 1 })
+      expect(Object.keys(result).length).to.equal(1)
+      expect(result.test).to.equal(1)
+    })
+
+    it('merges number float values in', () => {
+      const result = ObjectHelpers.merge({ test: 'value'} , { test: 1.2 })
+      expect(Object.keys(result).length).to.equal(1)
+      expect(result.test).to.equal(1.2)
+    })
+
+    it('merges date values in', () => {
+      const dateValue = new Date()
+      const result = ObjectHelpers.merge({ test: 'value'} , { test: dateValue})
+      expect(Object.keys(result).length).to.equal(1)
+      expect(result.test.toISOString()).to.equal(dateValue.toISOString())
+    })
+
+    it('merges nested objects in', () => {
+      const result = ObjectHelpers.merge({ test: { first: 'value' } } , { test: { second: 'value2' } })
+      expect(Object.keys(result).length).to.equal(1)
+      expect(result.test).to.be.a('object')
+      expect(result.test.first).to.equal('value')
+      expect(result.test.second).to.equal('value2')
+    })
+
+  })
 
 });
